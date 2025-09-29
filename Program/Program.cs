ae094b8be55172745dd0cbde0d84a0193a4e53e5
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2025-09-29 13:46:17 +0200

    Påbörjad meny

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Dagboksapp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("--Menu--");
+                Console.WriteLine(" 1: Skriv ny anteckning \n 2: lista anteckningar \n 3: Sök antekning");
+                Console.WriteLine(" 4: Spara till fil \n 5: Läs från fil \n 6: avsluta");
+                string choice = Console.ReadLine();
+
+                switch (choice.Trim())
+                {
+                    case "1":
+                        {
+
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid input, försök igen");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
